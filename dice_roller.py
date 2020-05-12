efb6ed9d1ee025ef9fec33c292ddc9c713479446
bashangbasir <38761135+bashangbasir@users.noreply.github.com>
2020-05-12 22:39:28 +0800

    Now rolling dice twice

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,16 @@
-def main():
-  print('You rolled a die')
+import random
 
+def main():
+  #first argument in randint() method is start value, 2nd arg is end value
+  dice_rolls = 2
+  dice_sum = 0
+  
+  for i in range(dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dice_sum += roll
+  
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
